d8b538bf697806234b9877199fd62bb239552a16
weihl <weihl@kumai-inc.com>
2023-12-05 16:12:48 +0800

    fix desktop 关于 sqlite 的使用 1、设置使用持久化缓存 ，持久化缓存 db 文件，可以指定详细的文件存储路径 2、或选择内存缓存策略，IN_MEMORY 3、判断持久化数据库文件是否存在 并可以正常链接

--- composeApp/src/desktopMain/kotlin/Platform.jvm.kt
+++ composeApp/src/desktopMain/kotlin/Platform.jvm.kt
@@ -1,15 +1,22 @@
 import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.jdbc.ConnectionManager
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import org.wells.aeiou.database.AeiouDatabase
+import java.sql.DriverManager
 
-class JVMPlatform: Platform {
+class JVMPlatform : Platform {
     override val name: String = "Java ${System.getProperty("java.version")}"
 }
 
 actual fun getPlatform(): Platform = JVMPlatform()
 
 actual fun createDriver(): SqlDriver {
-    val driver: SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
-    AeiouDatabase.Schema.create(driver)
+    // "jdbc:sqlite:test.db" ，持久保存
+    // JdbcSqliteDriver.IN_MEMORY ,在缓存中有效
+    val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:aeiou.db")
+    //DriverManager.getConnection("jdbc:sqlite:aeiou.db")
+    runCatching {
+        AeiouDatabase.Schema.create(driver)
+    }
     return driver
 }
